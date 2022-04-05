43dc4d3bcbec24e5d0eb8d5d0f5743a1566b8565
Nagaraj Anusha (MS/ESD-NR8-XC) <Anusha.Jagadeesha@in.bosch.com>
2022-04-05 20:04:51 +0530

    Added functions

--- Sender/send_Streaming_BMS_values.cpp
+++ Sender/send_Streaming_BMS_values.cpp
@@ -32,16 +32,14 @@ bool sendSensorDataToRx()
 {
     bool isSensorDataReadSuccessfully = readSensorDataFromTxt("./Sender/sensorStreamlineData.txt");
     std::fstream outData;
-    outData.open("./Sender/sensorStreamlineOutData.txt",ios::out);
-    //outData << "SOC" << "," << "Temprature" << endl;
-    // for (int line = 0; line < MAX_BMS_STREAM_READINGS; line++)
-    // {
-    //     cout << m_prepareSensorInputToSender.SOC[line] << " , " << m_prepareSensorInputToSender.temperature[line] << endl;
-    //     //outData << m_prepareSensorInputToSender.SOC[line] << "," << m_prepareSensorInputToSender.temperature[line] << endl;
-    //     outData << "writing in this file.\n";
-    // }
-    cout << isSensorDataReadSuccessfully << endl;
-    outData << "writing in this file.\n"<<endl;
+    outData.open("sensorStreamlineOutData.txt",ios::out);
+    outData << "SOC" << "," << "Temprature" << endl;
+    for (int line = 0; line < MAX_BMS_STREAM_READINGS; line++)
+    {
+        cout << m_prepareSensorInputToSender.SOC[line] << " , " << m_prepareSensorInputToSender.temperature[line] << endl;
+        outData << m_prepareSensorInputToSender.SOC[line] << "," << m_prepareSensorInputToSender.temperature[line] << endl;
+    }
+    //outData.write("writing in this file.\n", sizeof("writing in this file.")); 
     outData.close();
     return isSensorDataReadSuccessfully;
 }
