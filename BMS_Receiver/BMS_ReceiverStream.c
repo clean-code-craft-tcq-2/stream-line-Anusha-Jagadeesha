1edc999da73ca5c9386cd6cdc5dea785d33b8914
Suchitha N M <90901005+SuchithaNM@users.noreply.github.com>
2022-04-11 18:13:51 +0530

    Update BMS_ReceiverStream.c

--- BMS_Receiver/BMS_ReceiverStream.c
+++ BMS_Receiver/BMS_ReceiverStream.c
@@ -1,8 +1,10 @@
 #include "BMS_ReceiverStream.h"
 
+float min, max;
 float Temp[BMS_DATA] = {};
 float Soc[BMS_DATA] = {};
-float min, max;
+
+int NoOfEnteries = 5;
 
 void getDataFromConsole()
 {
@@ -12,24 +14,24 @@ void getDataFromConsole()
 	}
 }
 
-float minBMSParameterValue(float Temp[] , int size)
+float minBMSParameterValue(float BatteryParameter[] , int size)
 {
-	min = Temp[0];
-	for (int i = 0; i < BMS_DATA; i++)
+	min = BatteryParameter[0];
+	for (int i = 0; i < size; i++)
 	{
-		if (Temp[i] < min)
-			min = Temp[i];
+		if (BatteryParameter[i] < min)
+			min = BatteryParameter[i];
 	}
 	return min;
 }
 
-float maxBMSParameterValue(float Temp[] , int size)
+float maxBMSParameterValue(float BatteryParameter[] , int size)
 {
-	max = Temp[0];
-	for (int i = 0; i < BMS_DATA; i++)
+	max = BatteryParameter[0];
+	for (int i = 0; i < size; i++)
 	{
-		if (Temp[i] > max)
-			max = Temp[i];
+		if (BatteryParameter[i] > max)
+			max = BatteryParameter[i];
 	}
 	return max;
 }
@@ -49,13 +51,14 @@ float calculateSMA(float BatteryParameter[], int NoOfEnteries)
 }
 void printData()
 {
+	
 	float SocSMA = calculateSMA(Soc ,NoOfEnteries);
 	printf("Simple moving average of Temp : %0.2lf \n ", SocSMA);
 	float TempSMA = calculateSMA(Temp ,NoOfEnteries);
 	printf("Simple moving average of SOC : %0.2lf \n ",TempSMA);
 
-	printf("min temp :%f\n", minBMSParameterValue(Temp,BMS_DATA));
-	printf("max temp :%f\n", maxBMSParameterValue(Temp,BMS_DATA));
-	printf("min Soc:%f\n",  minBMSParameterValue(Soc,BMS_DATA));
-	printf("max Soc :%f\n", maxBMSParameterValue(Soc,BMS_DATA));
+	printf("min temp :%f\n", minBMSParameterValue(Temp, BMS_DATA));
+	printf("max temp :%f\n", maxBMSParameterValue(Temp, BMS_DATA));
+	printf("min Soc:%f\n",  minBMSParameterValue(Soc, BMS_DATA));
+	printf("max Soc :%f\n", maxBMSParameterValue(Soc, BMS_DATA));
 }
