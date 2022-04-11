69a1ffc9ae3d0c62f3ec497bfc25e24ff060232a
Suchitha N M <90901005+SuchithaNM@users.noreply.github.com>
2022-04-11 18:17:26 +0530

    Update BMS_ReceiverStream.h

--- BMS_Receiver/BMS_ReceiverStream.h
+++ BMS_Receiver/BMS_ReceiverStream.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -9,3 +11,7 @@ extern float minBMSParameterValue(float BatteryParameter[] , int size);
 extern float maxBMSParameterValue(float BatteryParameter[] , int size);
 extern float calculateSMA(float BatteryParameter[], int NoOfEnteries);
 extern void printData();
+
+
+extern float Temp[BMS_DATA];
+extern float Soc[BMS_DATA];
