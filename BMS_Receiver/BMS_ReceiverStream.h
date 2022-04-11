9e5395b8e90c80c5b8fba8ea96f24f71252a1a17
Suchitha N M <90901005+SuchithaNM@users.noreply.github.com>
2022-04-11 18:09:00 +0530

    Update BMS_ReceiverStream.h

--- BMS_Receiver/BMS_ReceiverStream.h
+++ BMS_Receiver/BMS_ReceiverStream.h
@@ -1,1 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
+#define BMS_DATA 50
+
+extern void getDataFromConsole();
+extern float minBMSParameterValue(float Temp[] , int size);
+extern float maxBMSParameterValue(float Temp[] , int size);
+extern float calculateSMA(float BatteryParameter[], int NoOfEnteries);
+extern void printData();
