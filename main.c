000225aaafefa26288d48cb70398fca6629cf3c4
Suchitha N M <90901005+SuchithaNM@users.noreply.github.com>
2022-04-12 22:33:48 +0530

    Create main.c

---
+++ main.c
@@ -0,0 +1,10 @@
+#include "BMS_Receiver/BMS_ReceiverStream.h"
+#include "Sender/send_Streaming_BMS_values.h"
+"
+int main()
+{
+  sendSensorDataToRx
+  readDataFromConsole();
+  printData();
+  return 0;
+}
